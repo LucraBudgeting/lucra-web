a5e61ec708643828056624199db9c9468d64715d
Ryan H <ryanzhutch@gmail.com>
2024-04-21 11:50:25 -0600

    Add new files and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,51 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import react from '@vitejs/plugin-react-swc';
-import { qrcode } from 'vite-plugin-qrcode';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3030,
   },
+
   preview: {
     port: 8080,
   },
-  plugins: [react(), qrcode()],
+
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically check for updates
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
+            options: {
+              // Configure cache options here if needed
+            },
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+  },
+
+  build: {
+    sourcemap: true,
+
+    rollupOptions: {
+      onLog(level, log, handler) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        if (log.cause && log.cause.message === `Can't resolve original location of error.`) {
+          return;
+        }
+        handler(level, log);
+      },
+    },
   },
 });
