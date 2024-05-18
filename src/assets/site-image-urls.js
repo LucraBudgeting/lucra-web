8483c0dc965bd6e18360ba6566c4b3cacab2c1a0
Ryan H <ryanzhutch@gmail.com>
2024-05-18 17:10:32 -0600

    feat: Add site-image-urls.js with image URLs for login, logo, onboarding, and more

---
+++ src/assets/site-image-urls.js
@@ -0,0 +1,15 @@
+export const siteImageUrls = {
+  login_desktop_banner:
+    'https://res.cloudinary.com/dr8cg8xmb/image/upload/site-images/login-desktop-banner.png',
+  login_mobile_banner:
+    'https://res.cloudinary.com/dr8cg8xmb/image/upload/site-images/login-mobile-banner.png',
+  logo_primary: 'https://res.cloudinary.com/dr8cg8xmb/image/upload/site-images/logo-primary.png',
+  logo_secondary:
+    'https://res.cloudinary.com/dr8cg8xmb/image/upload/site-images/logo-secondary.png',
+  onboarding_card:
+    'https://res.cloudinary.com/dr8cg8xmb/image/upload/site-images/onboarding-card-front.png',
+  onboarding_lock:
+    'https://res.cloudinary.com/dr8cg8xmb/image/upload/site-images/onboarding-lock.png',
+  onboarding_boy:
+    'https://res.cloudinary.com/dr8cg8xmb/image/upload/site-images/onboarding-boy.png',
+};
