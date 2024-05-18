1cb2f55aa069c8d454a705d2c52f0fcccaee79bf
Ryan H <ryanzhutch@gmail.com>
2024-05-18 17:57:17 -0600

    feat: Update Storybook configuration and dependencies
    - Update .storybook/main.ts to include additional addons and remove unused autodocs configuration
    - Add ProgressBar.stories.tsx file with stories for different pages
    - Update package.json to update Storybook and related addons to the latest versions
    - Add ProgressBar component to src/atoms/progress/ProgressBar.tsx for displaying progress

---
+++ src/atoms/progress/ProgressBar.tsx
@@ -0,0 +1,66 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+import { FC } from 'react';
+import styled from 'styled-components';
+
+interface ProgressBarProps {
+  totalPages: number;
+  currentPage: number;
+}
+
+const height = '2px';
+
+export const ProgressBar: FC<ProgressBarProps> = ({ totalPages, currentPage }) => {
+  const [parentRef] = useAutoAnimate();
+  const getProgressWidth = () => {
+    return (currentPage / totalPages) * 100;
+  };
+
+  return (
+    <Styled.container>
+      <Styled.stepText>
+        <Styled.stepTextPrimary>Step {currentPage}</Styled.stepTextPrimary>
+        <Styled.stepTextSecondary> of {totalPages}</Styled.stepTextSecondary>
+      </Styled.stepText>
+      <Styled.progressBar>
+        <Styled.filledBar width={`${getProgressWidth()}%`} ref={parentRef} />
+      </Styled.progressBar>
+    </Styled.container>
+  );
+};
+
+const Styled = {
+  container: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%
+  `,
+  stepText: styled.div`
+    width: 30%;
+    display: flex;
+    gap: 4px;
+
+    h3 {
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 22px;
+    }
+  `,
+  stepTextPrimary: styled.h3`
+    color: #333;
+  `,
+  stepTextSecondary: styled.h3`
+    color: #b7b7b7;
+  `,
+  progressBar: styled.div`
+    width: 100%;
+    background-color: #e0e0e0;
+    height: ${height};
+  `,
+  filledBar: styled.div<{ width: string }>`
+    width: ${(props) => props.width};
+    height: ${height};
+    background-color: #333333;
+  `,
+};
---
+++ src/stories/ProgressBar.stories.tsx
@@ -0,0 +1,59 @@
+import { ProgressBar } from '@/atoms/progress/ProgressBar';
+import type { Meta, StoryObj } from '@storybook/react';
+import { ParentContainer } from './ParentContainer';
+
+const meta = {
+  title: 'atom/ProgressBar',
+  component: ProgressBar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {},
+  decorators: [(story) => <ParentContainer width="800px">{story()}</ParentContainer>],
+} satisfies Meta<typeof ProgressBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+      currentPage: 1,
+    totalPages: 6,
+  },
+};
+
+export const page2: Story = {
+  args: {
+      currentPage: 2,
+    totalPages: 6,
+  },
+};
+
+export const page3: Story = {
+  args: {
+      currentPage: 3,
+    totalPages: 6,
+  },
+};
+
+export const page4: Story = {
+  args: {
+      currentPage: 4,
+    totalPages: 6,
+  },
+};
+
+export const page5: Story = {
+  args: {
+      currentPage: 5,
+    totalPages: 6,
+  },
+};
+
+export const page6: Story = {
+  args: {
+      currentPage: 6,
+    totalPages: 6,
+  },
+};
