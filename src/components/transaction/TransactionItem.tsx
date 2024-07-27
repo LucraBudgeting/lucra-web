e6787dc059d2bd1e11b15f8036d00c9992a04894
Ryan H <ryanzhutch@gmail.com>
2024-07-27 17:47:26 -0600

    refactor: Improve TransactionItem component styles for long text and add overflow-x hidden

--- src/components/transaction/TransactionItem.tsx
+++ src/components/transaction/TransactionItem.tsx
@@ -32,6 +32,7 @@ const Styled = {
     width: 40%;
     text-overflow: ellipsis;
     white-space: nowrap;
+    overflow-x: hidden;
   `,
   amount: styled.p<{ amount: number }>`
     font-weight: 600;
