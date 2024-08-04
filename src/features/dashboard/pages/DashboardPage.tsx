2cce8ec3bae8e4ea53f9d2654d83c48374789e89
Ryan H <ryanzhutch@gmail.com>
2024-08-04 15:02:18 -0600

    refactor: Add lazy loading for images in Budgets and Transactions components

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 import { useCategories } from '@/hooks/dashboard/useCategories.hook';
-import { LoadingComponent } from '@/atoms/loading/Loading.Component';
 import { useTransactions } from '@/hooks/dashboard/useTransactions.hook';
 import { Budgets } from '../components/Budgets';
 import { Transactions } from '../components/Transactions';
@@ -16,20 +15,8 @@ export const DashboardPage: FC<DashboardPageProps> = ({}) => {
   return (
     <Styled.container>
       <SplitView
-        left={
-          isCategoriesFetching ? (
-            <LoadingComponent loadingText="Loading Budgets" />
-          ) : (
-            <Budgets categories={categories} />
-          )
-        }
-        right={
-          isTransactionsFetching ? (
-            <LoadingComponent loadingText="Loading Transactions" />
-          ) : (
-            <Transactions transactions={transactions} />
-          )
-        }
+        left={<Budgets categories={categories} isFetching={isCategoriesFetching} />}
+        right={<Transactions transactions={transactions} isFetching={isTransactionsFetching} />}
       />
     </Styled.container>
   );
