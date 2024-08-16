c052e5cb5d2fdf920f5c61b21ce1ddef903856be
Ryan H <ryanzhutch@gmail.com>
2024-08-15 19:28:43 -0600

    package update

--- src/types/location/Address.ts
+++ src/types/location/Address.ts
@@ -1,4 +1,3 @@
-import { FieldValues } from 'react-hook-form';
 import { stateAbbreviation, statesFull } from './States';
 
 export class Address {
@@ -9,16 +8,6 @@ export class Address {
   postalCode = '';
   city = '';
   state: statesFull | stateAbbreviation | string = stateAbbreviation.Arkansas;
-
-  fromFieldValues = (fieldValues: FieldValues) => {
-    this.street1 = fieldValues.street1;
-    this.street2 = fieldValues.street2;
-    this.postalCode = fieldValues.postalCode;
-    this.city = fieldValues.city;
-    this.state = fieldValues.state;
-
-    return this;
-  };
 
   constructor(params?: Partial<Address>) {
     Object.assign(this, params);
